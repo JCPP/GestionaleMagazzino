ded3fd7ddc23ac94d960712a91ca9a7fba10914b
Davide Pastore <pasdavide@gmail.com>
2013-07-01 09:35:20 +0200

    Aggiunto qualche caso di testing per l'emailValidator

--- test/gestionaleCancelleria/EmailValidatorTest.java
+++ test/gestionaleCancelleria/EmailValidatorTest.java
@@ -8,20 +8,35 @@ import org.junit.Test;
 public class EmailValidatorTest {
 	
 	private EmailValidator emailValidator;
-	private String email;
+	private String emailFalse[]; //Contiene tutte le email per le quali il risultato sarà false
+	private String emailTrue[]; //Contiene tutte le email per le quali il risultato sarà true
 	
 	@Before
 	public void before(){
 		emailValidator = new EmailValidator();
+		emailFalse = new String[]{
+			"", 					//Stringa vuota
+			"asd",					//Stringa senza @
+			"asd@gmail.comasd@sd",	//Stringa con doppia @
+			"asd@sadasd",			//Stringa senza .
+			"asd @asdasd.cs"		//Indirizzo con spazio
+		};
+		
+		emailTrue = new String[]{
+			"asd@asd.com",			//Indirizzo email normale
+			"qwerty1@asd.com"		//Indirizzo email con numero
+		};
 	}
 
 	@Test
 	public void testValidate() {
-		email = "";
-		assertFalse("Controllo email errato", emailValidator.validate(email));
+		for (String email : emailFalse){
+			assertFalse("Controllo email errato", emailValidator.validate(email));
+		}
 		
-		email = "asd";
-		assertFalse("Controllo email errato", emailValidator.validate(email));
+		for (String email : emailTrue){
+			assertTrue("Controllo email errato", emailValidator.validate(email));
+		}
 	}
 
 }
