baecce995d26216691b00eb3b11f0fbc3b12afa3
Matteo Calò <emmeci92@gmail.com>
2013-07-02 13:16:41 +0200

    Closed #8

---
+++ src/gestionale/magazzino/Carrello.java
@@ -0,0 +1,23 @@
+package gestionale.magazzino;
+
+import java.util.ArrayList;
+
+public class Carrello {
+	private ArrayList<Acquisto> acquisti;
+
+
+	/**
+	 * @return the acquisti
+	 */
+	public ArrayList<Acquisto> getAcquisti() {
+		return acquisti;
+	}
+
+	/**
+	 * @param acquisti the acquisti to set
+	 */
+	public void setAcquisti(ArrayList<Acquisto> acquisti) {
+		this.acquisti = acquisti;
+	}
+	
+}
