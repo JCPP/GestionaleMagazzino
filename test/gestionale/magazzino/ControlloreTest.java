144fa15b2dc5aa0ee54f28c6621fca058ab55228
Davide Pastore <pasdavide@gmail.com>
2013-07-02 10:33:07 +0200

    Closed #10, sistemati i riferimenti nei test.

--- test/gestionale/magazzino/ControlloreTest.java
+++ test/gestionale/magazzino/ControlloreTest.java
@@ -1,6 +1,7 @@
-package gestionaleCancelleria;
+package gestionale.magazzino;
 
 import static org.junit.Assert.*;
+import gestionale.magazzino.Controllore;
 
 import org.junit.Before;
 import org.junit.Test;
